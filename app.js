526929822a1066f3f8b69dcc0f2bc68b819ed68c
Moo778 <a.t.marutani.cole@gmail.com>
2017-06-20 15:00:20 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function eatHumans(bearName, humanNames){
+	console.log(bearName);
+	console.log(humanNames);\
+	console.log(bearName + "ate" + humanNames[2])
+}
+
+eatHumans("Alexander", ["Bob", "Jill", "Bill"]);
